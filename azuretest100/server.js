249365cced709cf292b1be0820b802a4f6ca09a3
moetime <morrice_pfeiffer@hotmail.com>
2016-10-27 18:12:57 -0400

    Github commit

--- azuretest100/server.js
+++ azuretest100/server.js
@@ -1,6 +1,7 @@
 ﻿var http = require( 'http' );
 http.createServer( function ( req, res )
 {
+
     console.log( 'Got request for ' + req.url );
     res.writeHead( 200, { 'Content-Type': 'text/html' } );
     res.end( '<h1>Hello Code and Azure Web Apps!</h1>' );
